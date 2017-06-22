3aaaa3e9163ff3f31cd604dd4def613667066809
sbx320 <sbx320@sbx320.net>
2017-06-22 03:30:34 +0200

    Initial Protobufs (22.06.2017)

---
+++ generate.bat
@@ -0,0 +1,6 @@
+ProtobufDumper "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\bin\win64\client.dll" dota proto.dota
+ProtobufDumper "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\bin\win64\server.dll" dota proto.dota
+ProtobufDumper "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\bin\win64\host.dll" dota proto.dota
+
+ProtobufDumper "C:\Program Files (x86)\Steam\steamclient.dll" steam proto.steam
+
